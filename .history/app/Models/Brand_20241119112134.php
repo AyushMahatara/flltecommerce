7e275e8ff6c6953f1ccbc23413150a65250f359f
Ayush Mahatara <ayush.mahatara@inlingtoncollege.edu.np>
2024-11-19 16:53:51 +0545

    first commit

---
+++ .history/app/Models/Brand_20241119112134.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    //
+
+    protected $table = 'brands';
+
+    protected $guarded = [];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
---
+++ app/Models/Address.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    protected $table = 'addresses';
+
+    protected $guarded = [];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+}
