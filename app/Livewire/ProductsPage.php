c56bd8c428f4d30f0295869407f1a0b030189409
Ayush Mahatara <ayush.mahatara10@gmail.com>
2024-12-08 10:29:04 +0545

    filter per category

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -6,15 +6,23 @@ use App\Models\Brand;
 use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
+use Livewire\Attributes\Url;
+use Livewire\WithPagination;
 use Livewire\Attributes\Title;
 
 #[Title('Product Page')]
 
 class ProductsPage extends Component
 {
+    use WithPagination;
+    #[Url]
+    public $selected_categories = [];
     public function render()
     {
         $products = Product::where('is_active', 1)->with('media')->paginate(6);
+        if (!empty($this->selected_categories)) {
+            $products = Product::where('is_active', 1)->whereIn('category_id', $this->selected_categories)->with('media')->paginate(6);
+        }
         $categories = Category::where('status', 1)->get(['id', 'name', 'slug']);
         $brands = Brand::where('status', 1)->get(['id', 'name', 'slug']);
 
