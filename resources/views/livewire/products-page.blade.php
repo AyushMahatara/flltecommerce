1ed5cee1169ea5fd270dfa9fb718a296a27af100
Ayush Mahatara <ayush.mahatara10@gmail.com>
2024-12-08 12:49:53 +0545

    filter by price

--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -56,14 +56,17 @@
 
                     <div class="p-4 mb-5 bg-white border border-gray-200 dark:bg-gray-900 dark:border-gray-900">
                         <h2 class="text-2xl font-bold dark:text-gray-400">Price</h2>
-                        <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
+                        <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400">{{
+                            Number::currency($price_range, 'NRP')}}</div>
                         <div>
                             <input type="range"
-                                class="w-full h-1 mb-4 bg-blue-100 rounded appearance-none cursor-pointer" max="500000"
-                                value="100000" step="100000">
+                                class="w-full h-1 mb-4 bg-blue-100  rounded appearance-none cursor-pointer"
+                                wire:model.live='price_range' max="500000" value="300000" step="1000">
                             <div class="flex justify-between ">
-                                <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 1000</span>
-                                <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 500000</span>
+                                <span class="inline-block text-lg font-bold text-blue-400 ">{{ Number::currency(1000,
+                                    'NRP') }}</span>
+                                <span class="inline-block text-lg font-bold text-blue-400 ">{{ Number::currency(500000,
+                                    'NRP') }}</span>
                             </div>
                         </div>
                     </div>
